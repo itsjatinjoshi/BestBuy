19fad8bdce81329e9e9871bcefa6273b62434dcd
Jatin-PC <itsjatinjoshi>
2020-08-18 20:52:33 -0400

    setup function to run test with chrome, firefox and IE

--- TestCases/conftest.py
+++ TestCases/conftest.py
@@ -5,11 +5,30 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 import time
 
+driver = None
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name", action="store", default="chrome"
+    )
+
 
 @pytest.fixture(scope="class")
 def setup_chrome_browser(request):
-    driver = webdriver.Chrome(
-        executable_path="C:\\Users\Jatin-PC\Desktop\Drivers\chromedriver_win32\chromedriver.exe")
+    global driver
+    browser_name = request.config.getoption("browser_name")
+    if browser_name == "chrome":
+        driver = webdriver.Chrome(
+            executable_path="C:\\Users\Jatin-PC\Desktop\Drivers\chromedriver_win32\chromedriver.exe")
+
+    elif browser_name == "firefox":
+        # set path of firefox with gecko driver
+        pass
+    elif browser_name == "IE":
+        # set the path of IE driver
+        pass
+
     driver.get("https://www.bestbuy.ca/en-ca")
     driver.maximize_window()
     print(driver.title)
